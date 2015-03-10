1a479a5e76c8cee355e10bccb0f1b8be3b922da4
John McDowall <john@mcdowall.info>
2015-03-10 16:18:29 -0700

    Devise Invitable and error pages

--- lib/generators/dresssed/devise_generator.rb
+++ lib/generators/dresssed/devise_generator.rb
@@ -5,8 +5,6 @@ module Dresssed
     class DeviseGenerator < Rails::Generators::Base
       include HandlerSupport
 
-      #source_root File.expand_path('../../../app/views/devise', __FILE__)
-
       desc "Copy Dresssed's custom Devise layouts to your project."
 
       def copy_layouts
