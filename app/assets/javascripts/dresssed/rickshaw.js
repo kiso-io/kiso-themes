3d7471b7084163e3d85ad7a6251dcfa127f3fb52
John McDowall <john@mcdowall.info>
2015-04-03 22:31:33 -0700

    Rickshaw graphs

---
+++ app/assets/javascripts/dresssed/rickshaw.js
@@ -0,0 +1,3 @@
+//= require rickshaw/d3
+//= require rickshaw/rickshaw
+
--- test/dummy/app/assets/javascripts/graphs.js
+++ test/dummy/app/assets/javascripts/graphs.js
@@ -1,6 +1,42 @@
 //= require graph_data_generator
 
-$(document).ready(function(){
+function rickshawBars() {
+  var seriesData = [ [], [] ];
+  var random = new Rickshaw.Fixtures.RandomData(50);
+
+  for (var i = 0; i < 50; i++) {
+    random.addData(seriesData);
+  }
+
+  var graph = new Rickshaw.Graph( {
+    element: document.getElementById("rickshaw-bars"),
+    height: 200,
+    renderer: 'bar',
+    series: [
+      {
+        color: "#c72929",
+        data: seriesData[0],
+      }, {
+        color: "#90caf9",
+        data: seriesData[1],
+      }
+    ]
+  } );
+
+  graph.render();
+
+  $(window).on('resize', function(){
+    graph.configure({
+      width: $("#rickshaw-bars").parent('.panel-body').width(),
+      height: 200
+    });
+    graph.render();
+  });
+
+  var hoverDetail = new Rickshaw.Graph.HoverDetail( { graph: graph } );
+}
+
+function flotRealtime() {
   var dataGenerator = new DataGenerator(200);
   var plot = $.plot("#realtime", [ dataGenerator.getRandomizedData() ], {
     series: {
@@ -18,8 +54,13 @@ $(document).ready(function(){
   function update() {
     plot.setData([dataGenerator.getRandomizedData()]);
     plot.draw();
-    setTimeout(update, 35);
+    setTimeout(update, 24);
   }
 
   update();
+}
+
+$(document).ready(function(){
+  flotRealtime();
+  rickshawBars();
 });
