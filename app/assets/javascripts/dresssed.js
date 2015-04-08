563e26c433a17a053ea49c3e3c4ff8a12245424c
John McDowall <john@mcdowall.info>
2015-04-07 18:54:29 -0700

    Sidenav generator tweaks

--- app/assets/javascripts/dresssed.js
+++ app/assets/javascripts/dresssed.js
@@ -1,1 +1,13 @@
 //= require_tree ./dresssed
+
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover({
+    container: 'body'
+  });
+
+  $('[data-toggle="tooltip"]').tooltip({
+    container: 'body'
+  });
+
+  $('.nav-side-menu').metisMenu();
+});
