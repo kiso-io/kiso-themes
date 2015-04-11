c1327416647a60131e6189f54f387f7a70055c01
John McDowall <john@mcdowall.info>
2015-04-10 19:14:32 -0700

    Modal on load for dummies

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -32,19 +32,37 @@ $(document).ready(function(){
 
   sizeiframe(document.body.clientWidth, true);
 
+  $('#welcome-modal').on('hidden.bs.modal', function (e) {
+    setCookie('seen-modal', true);
+  })
+
+  if(!getCookie('seen-modal')) {
+    $('#welcome-modal').modal('show');
+  }
+
   $(window).on('resize', function(){
-      var $sgViewport = $('#sg-viewport');
-      var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      $($sgViewport).height(h-55);
-      sizeiframe(document.body.clientWidth);
-  });
-
-});
-
-function sizeiframe(size) {
     var $sgViewport = $('#sg-viewport');
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     $($sgViewport).height(h-55);
-    $('#sg-gen-container,#sg-viewport').addClass("vp-animate");
-    $sgViewport.width(size);
+    sizeiframe(document.body.clientWidth);
+  });
+});
+
+function sizeiframe(size) {
+  var $sgViewport = $('#sg-viewport');
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  $($sgViewport).height(h-55);
+  $('#sg-gen-container,#sg-viewport').addClass("vp-animate");
+  $sgViewport.width(size);
 };
+
+function setCookie(key, value) {
+  var expires = new Date();
+  expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+  document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+  return keyValue ? keyValue[2] : null;
+}
