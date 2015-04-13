214bb0e4ae0552abfa2a2972a374109d15d9418f
John McDowall <john@mcdowall.info>
2015-04-13 12:22:12 -0700

    RC.22  - Reinstated the haml views for devise

--- lib/dresssed/ives/version.rb
+++ lib/dresssed/ives/version.rb
@@ -1,6 +1,6 @@
 module Dresssed
   module Ives
-    VERSION = "1.0.0.rc21"
+    VERSION = "1.0.0.rc22"
     STYLES = %w(blue black amber)
   end
 end
--- lib/generators/dresssed/devise_generator.rb
+++ lib/generators/dresssed/devise_generator.rb
@@ -8,7 +8,7 @@ module Dresssed
       desc "Copy Dresssed's custom Devise layouts to your project."
 
       def copy_layouts
-        directory File.expand_path('../../../../app/views/devise', __FILE__), 'app/views/devise'
+        directory File.expand_path("../../../../app/views/devise/#{handler}", __FILE__), 'app/views/devise'
       end
     end
   end
