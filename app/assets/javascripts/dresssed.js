00a7c0de34fec9ce4e0f4752a76bf30fc9ae1693
John McDowall <john@mcdowall.info>
2015-04-22 17:21:25 -0700

    Fix the slimscroll breaking the sidenav collapse when the browser window is wanked back and forth

--- app/assets/javascripts/dresssed.js
+++ app/assets/javascripts/dresssed.js
@@ -24,6 +24,28 @@ $(document).ready(function(){
      });
   }
 
+  // AHOY THERE!
+  //
+  // This code exists purely for the situation of when the demo
+  // app hosts the Ives theme. Slimscroll will prevent the sidenav
+  // bar from collapsing correctly ONLY in the circumstance that
+  // someone is wanking the browser window back and forth.
+  //
+  // It is safe to delete this code yourself.
+  $(window).on('resize', function(){
+    var width = document.body.clientWidth;
+    if(width<800) {
+      $('#menu-content').height(0);
+      $('#menu-content').slimScroll({
+           destroy: 'true'
+       });
+    } else if( width > 800) {
+      $('#menu-content').slimScroll({
+           height: '100%'
+       });
+    }
+  });
+
   flotMetric($('#metric-monthly-earnings'), [
       [0, 4],
       [1, 8],
