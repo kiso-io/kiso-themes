097b9303f73df1291fc7032b437d44ab45911aa2
John McDowall <john@mcdowall.info>
2015-04-22 20:58:14 -0700

    Neutralize devise forms in demo and fix that ZeroClipboard crap

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '2.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( styles/blue.css styles/amber.css styles/black.css font-awesome.css ionicons.css icons-material-design.css bootstrap_docs.css blue.css)
+Rails.application.config.assets.precompile += %w( styles/blue.css styles/amber.css styles/black.css font-awesome.css ionicons.css icons-material-design.css bootstrap_docs.css blue.css flash/ZeroClipboard.swf)
