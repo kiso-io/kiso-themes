b86f2d09cc9624718867ffe69a3e86c198d02428
John McDowall <john@mcdowall.info>
2016-06-05 20:28:34 -0700

    Make sure flash type is always a Symbol in the bootstrap_class_for helper

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -42,6 +42,6 @@ module HtmlHelper
   end
 
   def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 end
