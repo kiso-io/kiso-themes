0e249b4ebdad14eaec9996b0145ec1aa4ea583f4
John McDowall <john@mcdowall.info>
2016-06-05 18:55:20 -0700

    Restore bootstrap_class_for helper

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -41,4 +41,7 @@ module HtmlHelper
     content_tag :pre, code, :class => "prettyprint linenums"
   end
 
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+  end
 end
