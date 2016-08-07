f89065877cf93c81d762846c47e42deb3f35903d
John McDowall <john@mcdowall.info>
2016-08-07 14:41:10 -0700

    Dashboard generator update

--- lib/generators/dresssed/sidenav_generator.rb
+++ lib/generators/dresssed/sidenav_generator.rb
@@ -6,7 +6,7 @@ module Dresssed
       view_name "sidenav"
 
       def set_layout
-        controller_const_name = "#{name}Controller".constantize
+        controller_const_name = "#{name.camelize}Controller".constantize
         inject_into_class File.join('app/controllers', class_path, "#{file_name}_controller.rb"), controller_const_name, "  layout 'sidenav'\n"
       end
     end
