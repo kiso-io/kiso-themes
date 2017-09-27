1df2c26b965c9151401e9a23cdae5c2815b82b9b
John McDowall <john@mcdowall.info>
2017-09-26 19:55:48 -0700

    inspector

--- app/assets/javascripts/inspect_mode/inspect_mode.js
+++ app/assets/javascripts/inspect_mode/inspect_mode.js
@@ -1,49 +1,5 @@
 window.INSPECT_MODE = false;
 
-// Parameters:
-// code                 - (string) code you wish to format
-// stripWhiteSpaces     - (boolean) do you wish to remove multiple whitespaces coming after each other?
-// stripEmptyLines      - (boolean) do you wish to remove empty lines?
-var formatCode = function(code, stripWhiteSpaces, stripEmptyLines) {
-    var whitespace          = ' '.repeat(2);             // Default indenting 4 whitespaces
-    var currentIndent       = 0;
-    var char                = null;
-    var nextChar            = null;
-
-
-    var result = '';
-    for(var pos=0; pos <= code.length; pos++) {
-        char            = code.substr(pos, 1);
-        nextChar        = code.substr(pos+1, 1);
-
-        // If opening tag, add newline character and indention
-        if(char === '<' && nextChar !== '/') {
-            result += '\n' + whitespace.repeat(currentIndent);
-            currentIndent++;
-        }
-        // if Closing tag, add newline and indention
-        else if(char === '<' && nextChar === '/') {
-            // If there're more closing tags than opening
-            if(--currentIndent < 0) currentIndent = 0;
-            result += '\n' + whitespace.repeat(currentIndent);
-        }
-
-        // remove multiple whitespaces
-        else if(stripWhiteSpaces === true && char === ' ' && nextChar === ' ') char = '';
-        // remove empty lines
-        else if(stripEmptyLines === true && char === '\n' ) {
-            //debugger;
-            if(code.substr(pos, code.substr(pos).indexOf("<")).trim() === '' ) char = '';
-        }
-
-        result += char;
-    }
-
-    result = result.replace(/^[\r\n]+/, "")
-
-    return result;
-}
-
 $(document).ready(function() {
   var clipboard = new Clipboard('.copy-to-clipboard');
   clipboard.on('success', function(e) {
@@ -82,6 +38,11 @@ $(document).ready(function() {
           var rawHtml = $($inspectable)[0].outerHTML
 
           $('.inspect-mode-bg').removeClass('d-none').addClass('d-flex')
+
+          componentNameRe = /<!-- component-title: ([\w\s]*) -->/g
+          componentName = componentNameRe.exec(rawHtml)
+        
+          $('.inspector-component-name').text(componentName[1])
 
           rawHtml = rawHtml.trim().replace(/<!--[\s\S]*?-->/g, '')
 
