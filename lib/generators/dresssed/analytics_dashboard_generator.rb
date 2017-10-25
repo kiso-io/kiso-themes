dd883bc5d7be56b985ec6ab40b0cee6ed2def50c
John McDowall <john@mcdowall.info>
2017-10-25 13:11:20 -0700

    Analytics generators using _app_nav

--- lib/generators/dresssed/analytics_dashboard_generator.rb
+++ lib/generators/dresssed/analytics_dashboard_generator.rb
@@ -7,7 +7,7 @@ module Dresssed
 
       def set_layout
         controller_const_name = "#{name.camelize}Controller".constantize
-        inject_into_class File.join('app/controllers', class_path, "#{file_name}_controller.rb"), controller_const_name, "  layout 'application'\n"
+        inject_into_class File.join('app/controllers', class_path, "#{file_name}_controller.rb"), controller_const_name, "  layout '_app_nav'\n"
       end
     end
   end
--- lib/generators/dresssed/analytics_detail_generator.rb
+++ lib/generators/dresssed/analytics_detail_generator.rb
@@ -4,6 +4,11 @@ module Dresssed
   module Generators
     class AnalyticsDetailGenerator < PageGenerator
       view_name "analytics", "analytics_detail"
+
+      def set_layout
+        controller_const_name = "#{name.camelize}Controller".constantize
+        inject_into_class File.join('app/controllers', class_path, "#{file_name}_controller.rb"), controller_const_name, "  layout '_app_nav'\n"
+      end
     end
   end
 end
