f61def514750c0b6722d759c5d5f7f282c3a614e
John McDowall <john@mcdowall.info>
2017-10-26 13:02:01 -0700

    Convert all icon to theme_icon_tag helper

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -4,8 +4,8 @@ module IconsHelper
   #
   #   <%= fa_icon_tag "remove" %>
   #
-  def fa_icon_tag(name)
-    content_tag(:i, nil, class: "fa fa-#{name}")
+  def fa_icon_tag(name, opts={})
+    content_tag(:i, nil, class: "fa fa-#{name} #{opts[:class]}")
   end
 
   # Icon from Glyphicon Icons. Use the name without the 'glyphicon glyphicon-' prefix.
@@ -13,8 +13,8 @@ module IconsHelper
   #
   #   <%= glyphicon_tag "cloud-download" %>
   #
-  def glyphicon_tag(name)
-    content_tag(:i, nil, class: "glyphicon glyphicon-#{name}")
+  def glyphicon_tag(name, opts={})
+    content_tag(:i, nil, class: "glyphicon glyphicon-#{name} #{opts[:class]}")
   end
 
   # Icon from Ionicons. Use the name without the 'icon ion-' prefix.
@@ -22,8 +22,8 @@ module IconsHelper
   #
   #   <%= ionicon_icon_tag "remove" %>
   #
-  def ionicon_icon_tag(name)
-    content_tag(:i, nil, class: "icon ion-#{name}")
+  def ionicon_icon_tag(name, opts={})
+    content_tag(:i, nil, class: "icon ion-#{name} #{opts[:class]}")
   end
 
   # Icon from Material Design Icons.
@@ -31,8 +31,8 @@ module IconsHelper
   #
   #   <%= mdi_icon_tag "mdi-action-account-box" %>
   #
-  def mdi_icon_tag(name)
-    content_tag(:i, name, class: "material-icons")
+  def mdi_icon_tag(name, opts={})
+    content_tag(:i, name, class: "material-icons #{opts[:class]}")
   end
 
   # Icon from Themify Icon set.
@@ -40,8 +40,8 @@ module IconsHelper
   #
   #   <%= themify_icon_tag "alert" %>
   #
-  def themify_icon_tag(name)
-    content_tag(:i, nil, class: "ti-#{name}")
+  def themify_icon_tag(name, opts={})
+    content_tag(:i, nil, class: "ti-#{name} #{opts[:class]}")
   end
 
   # Provides a layer of indirection between the template
@@ -50,7 +50,7 @@ module IconsHelper
   #
   #   <%= theme_icon_tag "mdi-action-account-box" %>
   #
-  def theme_icon_tag(name)
-    themify_icon(name)
+  def theme_icon_tag(name, opts={})
+    themify_icon_tag(name, opts)
   end
 end
