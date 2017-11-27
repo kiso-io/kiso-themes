61394c6942d1d0cddfaf843b33112f678641d7d9
John McDowall <john@mcdowall.info>
2017-11-26 19:31:07 -0800

    Dashboard generator defaults

--- lib/generators/rrt/dashboard_pages_generator.rb
+++ lib/generators/rrt/dashboard_pages_generator.rb
@@ -6,6 +6,20 @@ module RRT
       view_name "dashboards","dashboard"
 
       namespace "rrt:dashboard_pages"
+
+      def set_layout
+        controller_const_name = "#{name.camelize}Controller".constantize
+        inject_into_class File.join('app/controllers', class_path, "#{file_name}_controller.rb"), controller_const_name, "  layout 'sidenav'\n"
+      end
+
+      def set_body_class_for_sidenav
+        controller_const_name = "#{name.camelize}Controller".constantize
+        inject_into_file "app/controllers/#{file_name}_controller.rb", after: "def index"  do
+<<-RUBY
+        \n    @body_class = "show-sidebar"
+RUBY
+        end
+      end
     end
   end
 end
