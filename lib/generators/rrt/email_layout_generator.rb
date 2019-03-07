b868a88a185c788225e890c96641e190fe1c6b08
John McDowall <john@mcdowall.info>
2019-03-06 17:30:26 -0800

    Email layout generator

---
+++ lib/generators/rrt/email_layout_generator.rb
@@ -0,0 +1,41 @@
+require "generators/rrt/handler_support"
+
+module RRT
+  module Generators
+    class EmailLayoutGenerator < Rails::Generators::Base
+      include HandlerSupport
+
+      source_root File.expand_path('../templates', __FILE__)
+
+      namespace "rrt:email_layout"
+      desc "Copy RRT's custom Email templates to your project."
+
+      def copy_layouts
+        abort "You must create a mailer first" and return unless mailer?
+        copy_file "views/emails/_email_header.html.#{handler}", "app/views/emails/_email_header.html.#{handler}"
+        copy_file "views/emails/_email_footer.html.#{handler}", "app/views/emails/_email_footer.html.#{handler}"
+      end
+
+      def add_helpers
+
+        code = <<-INJECTEDCODE
+
+  add_template_helper EmailTemplateHelper
+  include Rails.application.routes.url_helpers
+  include ActionView::Helpers::UrlHelper
+INJECTEDCODE
+
+        inject_into_file( "app/mailers/application_mailer.rb", code, :after => /^class ApplicationMailer < ActionMailer::Base/)
+
+        gsub_file 'app/mailers/application_mailer.rb', 'mailer', 'email'
+      end
+
+      protected
+
+      def mailer?
+        File.file?('app/mailers/application_mailer.rb')
+      end
+    end
+
+  end
+end
